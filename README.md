4a3c8b0e105642b4aa414b8f3938c82afa086312
Nachiket Dhamankar <nachiketdhamankar@hotmail.com>
2019-04-10 02:26:16 -0400

    Added forms

--- README.md
+++ README.md
@@ -6,8 +6,14 @@ I plan to watch the vidoes, create user stories and then try to code it on my ow
 
 ### User Stories
 - [x] I have a homepage which displays the posts, authors, date of when it was posted.
-- [ ] I have an access bar which has shortcuts.
-- [ ] I have a responsive layout which adapts to my screen size.
-- [ ] I can create my account using my email id.
-- [ ] I can login using my email id with my password.
+- [x] I have an access bar which has shortcuts.
+- [x] I have a responsive layout which adapts to my screen size.
+- [x] I can create my account using my email id.
+- [x] I can login using my email id with my password.
+- [x] Once I login, I get a success (or failure message) before redirecting to homepage (or staying on the same page)
+- [x] While logging in, if I fail to login, I see which field I entered wrong.
 - [ ] I can upload my display picture and have a default picture until I upload mine.
+- [ ] I can re-login and persist the data.
+
+#### Notes
+- Using wtforms for making forms -> You can make forms from classes
--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'b756a183bdd89016f0a397e7ca2785b0'
 posts = [
     {
         'author':"Author1",
@@ -26,6 +27,25 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title = "About")
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title = "Register", form = form)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@admin.com' and form.password.data == 'password':
+            flash(f'Sucessfully Logged in', 'success')
+            return redirect(url_for ('home'))
+        else:
+            flash(f'Try Again', 'danger')
+    return render_template('login.html', title = "Login", form = form)
 
 if __name__ == '__main__':
     app.run(debug=True)
